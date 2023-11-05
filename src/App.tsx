f0c0490dcb5f9205db4559a0469472ba9c9a53f6
Ben Himsel <ben.himsel@gmail.com>
2023-11-04 20:23:22 -0500

    Before pixijs refactor

--- src/App.tsx
+++ src/App.tsx
@@ -11,38 +11,21 @@ import "./App.css";
 import GameBoard from "./components/GameBoard";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [toggleBoard, setToggleBoard] = useState(false);
 
   return (
-    <div className="App">
+    <div className="App h-full w-full">
       <div className="w-full columns-3 bg-slate-600 rounded-sm">
         <div className="">a</div>
         <div className="">b</div>
         <div className="">c</div>
       </div>
-      <div id="board"></div>
-      <GameBoard></GameBoard>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+      <br />
+      <Button icon="pi pi-plus" className="mr-2" label="Show Board" onClick={() => setToggleBoard((toggleBoard) => !toggleBoard)}></Button>
+      <div id="board" className="justify-center w-full h-full">
+        {toggleBoard ? <GameBoard></GameBoard> : "Board Hidden"}
       </div>
-      <h1>Vite + PrimeReact</h1>
-      <div>
-        <h2>PrimeReact Typescript Issue Template</h2>
-        <p>Please create a test case and attach the link to the to your github issue report.</p>
-      </div>
-      <div className="card">
-        <Button icon="pi pi-plus" className="mr-2" label="Increment" onClick={() => setCount((count) => count + 1)}></Button>
-        <InputText value={count} />
-        <p>
-          Edit <code>src/App.tsx</code> and save to test PrimeReact
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
   );
 }
--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -2,34 +2,57 @@ import React, { useEffect, useRef } from "react";
 import { SVG } from "@svgdotjs/svg.js";
 import { defineHex, Grid, rectangle } from "honeycomb-grid";
 
-export class CustomHex extends defineHex({ dimensions: 20, origin: "topLeft" }) {}
+export class CustomHex extends defineHex({ dimensions: 20, origin: "topLeft" }) {
+  polygon: any;
+  child: any;
+}
 
 const GameBoard: React.FC = () => {
   const svgContainerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
+    svgContainerRef.current!.innerHTML = "";
+
     if (svgContainerRef.current) {
-      // Initialize the SVG drawing context
-      const draw = SVG().addTo(svgContainerRef.current).size("100%", "100%");
+      const draw = SVG().addTo(svgContainerRef.current).size(500, 500);
 
       const grid = new Grid(CustomHex, rectangle({ width: 10, height: 10 }));
 
-      // Render the SVG for each hex in the grid
+      svgContainerRef.current.addEventListener("click", ({ offsetX, offsetY }) => {
+        const clickedHex = grid.pointToHex({ x: offsetX, y: offsetY }, { allowOutside: false });
+        if (clickedHex) {
+          console.log(clickedHex.q);
+        }
+      });
+
       grid.forEach((hex) => {
         renderSVG(hex, draw);
       });
     }
   }, []);
 
-  const renderSVG = (hex: CustomHex, draw: any) => {
-    const polygon = draw
-      .polygon(hex.corners.map(({ x, y }) => `${x},${y}`))
-      .fill("none")
-      .stroke({ width: 1, color: "#999" });
-    draw.group().add(polygon);
+  const handleHexClick = (polygon: any, draw: any) => {
+    const currentFill = polygon.attr("fill");
+    const newFill = currentFill === "black" ? "#82EEFD" : "black";
+    polygon.fill(newFill);
   };
 
-  return <div ref={svgContainerRef} style={{ width: "100%", height: "100%" }} />;
+  const renderSVG = (hex: CustomHex, draw: any) => {
+    const points = hex.corners.map(({ x, y }) => `${x},${y}`).join(" ");
+    const polygon = draw
+      .polygon(points)
+      .fill("black")
+      .stroke({ width: 1, color: "#999" })
+      .click(() => handleHexClick(polygon, draw));
+
+    hex.polygon = polygon;
+  };
+
+  return (
+    <>
+      <div ref={svgContainerRef} className="h-full mx-auto justify-center items-center" />
+    </>
+  );
 };
 
 export default GameBoard;
